f922d87b935b94ee59391dbed1abc942f05a6ed6
Alvin Mites <alvin@mitesdesign.com>
2016-09-15 15:05:59 -0600

    added hello_world models

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
 
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+RATING_VALUES = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5), )
+
+
+class StuffToRate(models.Model):
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def get_rating(self):
+        total = 0
+        for rate in self.rating_set.all():
+            total += rate.rating
+        return total / self.rating_set.count()
+
+
+class Rating(models.Model):
+    stuff = models.ForeignKey(StuffToRate)
+    rating = models.IntegerField(default=1, choices=RATING_VALUES)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
