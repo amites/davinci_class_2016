40eeac3c7961b2cba834f49d54c8170dbdf48399
Alvin Mites <alvin@mitesdesign.com>
2016-09-27 13:14:19 -0600

    update to add a form to hello_world

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from hello_world.forms import RatingForm
 from hello_world.models import Rating, StuffToRate
 
 
 def index(request):
+    if request.method == 'POST':
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RatingForm()
     objs = StuffToRate.objects.all()
     thing = StuffToRate.objects.get(id=1)
     context = {
         'objs': objs,
         'giraffe': thing,
+        'form': form,
     }
     return render(request, 'base.html', context)
 
