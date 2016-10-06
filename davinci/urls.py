74eebc32d44119029a27ee45a606940c53180a72
Alvin Mites <alvin@mitesdesign.com>
2016-10-06 17:19:17 -0600

    Simplistic example of login / logout

--- davinci/urls.py
+++ davinci/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from hello_world import urls as hello_world_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello_world/', include(hello_world_urls)),
+
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'},
+        name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'},
+        name='logout'),
+    
 ]
