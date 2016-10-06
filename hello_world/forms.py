f1f1153d0e4322c79b96cf42331fe231c7141447
Alvin Mites <alvin@mitesdesign.com>
2016-10-06 11:47:58 -0600

    added rating field multiple choice

--- hello_world/forms.py
+++ hello_world/forms.py
@@ -4,6 +4,8 @@ from hello_world.models import Rating
 
 
 class RatingForm(forms.ModelForm):
+    rating = forms.MultipleChoiceField(required=False)
+
     class Meta:
         model = Rating
         fields = ['stuff', 'rating', ]
